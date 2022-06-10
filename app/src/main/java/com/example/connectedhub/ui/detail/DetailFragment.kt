6ee66c9066036f71c0cda4f46fd2525856f56971
GIFTS <gifts@GIFTSui-iMac.local>
2022-06-10 17:00:49 +0900

    otros detalles

--- app/src/main/java/com/example/connectedhub/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/connectedhub/ui/detail/DetailFragment.kt
@@ -1,6 +1,7 @@
 package com.example.connectedhub.ui.detail
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,12 +14,56 @@ import com.example.connectedhub.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment(R.layout.fragment_detail) {
     private lateinit var binding: FragmentDetailBinding
+    private var showLock = false
+    private var showUnlock = false
+    private var showEngine = false
     private val args by navArgs<DetailFragmentArgs>()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDetailBinding.bind(view)
         updateData()
 
+        clicks()
+
+    }
+
+    private fun clicks() {
+        binding.imgLockDoors.setOnClickListener {
+            showLock = !showLock
+            if (!showLock) {
+                binding.lockDoors.visibility = View.GONE
+            } else {
+                binding.lockDoors.visibility = View.VISIBLE
+            }
+        }
+        binding.imgStartEngine.setOnClickListener {
+            showEngine = !showEngine
+            if (!showEngine) {
+                binding.startEngine.visibility = View.GONE
+                binding.startEngine2.visibility = View.GONE
+            } else {
+                binding.startEngine.visibility = View.VISIBLE
+                binding.startEngine2.visibility = View.VISIBLE
+            }
+        }
+        binding.imgStartEngine2.setOnClickListener {
+            showEngine = !showEngine
+            if (!showEngine) {
+                binding.startEngine.visibility = View.GONE
+                binding.startEngine2.visibility = View.GONE
+            } else {
+                binding.startEngine.visibility = View.VISIBLE
+                binding.startEngine2.visibility = View.VISIBLE
+            }
+        }
+        binding.imgUnlockDoors.setOnClickListener {
+            showUnlock = !showUnlock
+            if (!showUnlock) {
+                binding.unlockDoors.visibility = View.GONE
+            } else {
+                binding.unlockDoors.visibility = View.VISIBLE
+            }
+        }
     }
 
     private fun updateData() {
